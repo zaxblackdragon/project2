3860e289a3e6c03cbe1b33bc273ceb682a7b09af
Zac Seewald <zaxblackdragon.com>
2018-12-17 23:06:40 -0600

    search.js capturing from the search html

--- public/js/search.js
+++ public/js/search.js
@@ -1,68 +1,74 @@
+$("#search-pet").on("click", function() {
+  
+
+
 function whenTheUserSubmitsASearch() {
-    //get everything they typed in the form, and put it in a data object
+   
     var dataObj = {};
     
-    var name = $("#pet-name-input").val().trim();
-    var age = $("#pet-age-input").val().trim();
+    var name = $("#search-pet-name").val().trim();
+    var age = $("#register-age").val().trim();
+    var status = $("#register-status").val().trim();
+    var pet_type = $("#register-type").val().trim();
+    var sex = $("#register-sex").val().trim();
+    var chip = $("#register-chip").val().trim();
+    var collartag = $("#register-collartag").val().trim();
+    var size = $("#register-size").val().trim();
+    var color = $("#register-color").val().trim();
+    var hair = $("#register-hair").val().trim();
+    var breed = $("#search-breed").val().trim();
+    var missingDays = $("#time-range").val().trim();
 
-    if (name !== undefined) {
+    if (name !== "") {
         dataObj.name = name
     }
 
-    if (age !== undefined) {
+    if (age !== "Age?") {
         dataObj.age = age
     }
 
-    if (status !== undefined) {
+    if (status !== "Lost, found or on the loose?") {
         dataObj.status = status
     }
 
-    if (age !== undefined) {
-        dataObj.age = age
+    if (pet_type !== "Pet Type?") {
+        dataObj.pet_type = pet_type
     }
 
-    if (pet_type !== undefined) {
-        dataObj.pet_type = pet_typ
-    }
-
-    if (sex !== undefined) {
+    if (sex !== "Sex?") {
         dataObj.sex = sex
     }
 
-    if (chip !== undefined) {
+    if (chip !== "Microchip?") {
         dataObj.chip = chip
     }  
 
-    if (collartag !== undefined) {
+    if (collartag !== "Collar and/or Tags?") {
         dataObj.collartag = collartag
     }
 
-    if (size !== undefined) {
+    if (size !== "Size?") {
         dataObj.size = size
     }  
     
-    if (color !== undefined) {
+    if (color !== "Color?") {
         dataObj.color = color
     }
 
-    if (hair !== undefined) {
+    if (hair !== "Hair?") {
         dataObj.hair = hair
     }  
     
-    if (breed !== undefined) {
+    if (breed !== "") {
         dataObj.breed = breed
     } 
-    
-    if (special !== undefined) {
-        dataObj.special = special
-    }
-
-    if (photolink !== undefined) {
-        dataObj.photolink = photolink
-    }
-
+    if (missingDays !== "Missing for:") {
+        dataObj.missingDays = missingDays
+    } 
 
     $.post("/api/search", dataObj, function(res){
         console.log("We did it");
     });
 }
+whenTheUserSubmitsASearch();
+});
